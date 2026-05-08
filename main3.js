b5e0576a9ee6320bfc5af4dcb259669b71b7de9f
Ransea <ransea475@gmail.com>
2026-05-08 11:23:31 +0700

    Create main3.js

---
+++ main3.js
@@ -0,0 +1,9 @@
+const apk = [
+ {
+  name: "Capcut Premium (Unlock Pro Version)",
+  version: "17.7.0",
+  size: "288",
+  tag: "Capcut",
+  urlApk: "https://www.mediafire.com/file/rkp15tzgv95gxtx/Zyvoid+x+CapCut+Premium+v17.7.0.apk/file"
+  },
+ ];
